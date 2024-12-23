e4d27f4a93f2d6e958ea4b172f8668dfb7119751
Premjeet <jeet4300795@gmail.com>
2024-12-23 14:12:33 +0530

    tested react icons

--- react-demos/src/components/Header.jsx
+++ react-demos/src/components/Header.jsx
@@ -1,9 +1,14 @@
+import { FaHome } from "react-icons/fa";
+
 const Header = () => {
   return (
     <header>
       <h1>Welcome to my Website</h1>
       <nav>
-        <a href="">Home</a>
+        <span className="flex flex-center">
+          <FaHome />
+          <a href="">Home</a>
+        </span>
         <a href="">About</a>
         <a href="">Contact</a>
       </nav>
